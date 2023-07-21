75bf16b1162da33256fbd9aa6757c3eeef99b1db
shagufthaanjum20 <shagufthanjum13@gmail.com>
2023-07-21 12:31:14 +0530

    first commit

--- app2/views.py
+++ app2/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def first(request):
+    return HttpResponse('<h1><marquee>i like to do funactivities</h2></marquee>')
